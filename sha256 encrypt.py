19d931a6934931e213653391d179a5547677d979
Azzam <48726420+omxqn@users.noreply.github.com>
2021-06-24 16:21:51 +0400

    Update sha256 encrypt.py

--- sha256 encrypt.py
+++ sha256 encrypt.py
@@ -5,22 +5,28 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from os import path
 from os import urandom
 from cryptography.fernet import Fernet
+import os
 key = b''
 
-if path.isfile("key.key"): # check if the key is exist
 
-    file = open('key.key', 'rb')  # Open the file as wb to read bytes
-    key = file.read()  # The key will be type bytes
-    file.close()
-    print("Your key has been successfuly imported \n\n")
-    
-    
+def check_key():
+    global key
+    if path.isfile("key.key"): # check if the key is exist
 
+        file = open('key.key', 'rb')  # Open the file as wb to read bytes
+        key = file.read()  # The key will be type bytes
+        file.close()
+        return True
+    else:
+        return False
+
+if check_key():
+    print("Your key has been successfuly imported \n\n")  
 else:
-    new_key()
-    
-    
-def new_key():
+    new_password()
+
+
+def new_password():
     global key
     password_provided = input("Enter your masterkey password: ")  # This is input in the form of a string
     
@@ -43,7 +49,7 @@ def new_key():
     print("Key has been saved",key)
 
 
-
+    
 def encrypting(message):
     global key
     message = message.encode() # turn it to bytes
@@ -69,18 +75,46 @@ def decrypting(message):# turn it to bytes
         return decrypted[2:len(decrypted)-1]
     except Exception as e:
         print("Invalid Key - Unsuccessfully decrypted")
-        
+        print(key)
         
    
         
 while True:
-    choice = str(input("Do you want to 'encrypt' or 'decrypt' ?: "))
-    if choice == "encrypt" or choice == "en":
-        message = input("Type the message that you want to encypt: ")
-        print(f"Key for word '{message}' is {encrypting(message)}")
+    if check_key():
+        choice = str(input("Do you want to 'encrypt' or 'decrypt' [help] for more?: "))
+        if choice == "encrypt" or choice == "en":
+            message = input("Type the message that you want to encypt: ")
+            print(f"Key for word '{message}' is \n {encrypting(message)}")
+
+            
+        if choice == "decrypt" or choice == "de":
+            encrypted_key = input("Enter the encrypted key must in '....': ")
+            print(decrypting(encrypted_key))
+
+        if choice == "key" or choice == "my_key" :
+            print("Your key is: {}".format(key))
+        
+        if choice.startswith("help"):
+            print("\n\n\n")
+            print("del,delete,change  -  Delete the current key \nhelp  - Get more help \nkey  - Display your key \nencrypt -  Encrypt your text")
+            print("\n\n\n")
 
         
-    if choice == "decrypt" or choice == "de":
-        encrypted_key = input("Enter the encrypted key must in '....': ")
+        if choice.startswith("del") or choice.startswith("change"):
+            while True:
+                ask = input("Are you sure you want to delete your key?: ")
+                ask = ask.lower()
+                if ask == "yes":
+                    print("Deleting .....")
+                    if path.isfile("key.key"):
+                      os.remove("key.key")
+                      print("Done \n")
+                    else:
+                      print("The file does not exist")
+                    break
 
-        print(decrypting(encrypted_key))
+                if ask == "no":
+                    print("Ok i won't")
+                    break
+    else:
+        new_password()
