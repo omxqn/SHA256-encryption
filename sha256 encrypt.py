b37b88dcac89b1cf33f03147b2dc6814bb0d5c08
Azzam <48726420+omxqn@users.noreply.github.com>
2021-07-13 20:21:19 +0400

    Update sha256 encrypt.py

--- sha256 encrypt.py
+++ sha256 encrypt.py
@@ -8,6 +8,15 @@ from cryptography.fernet import Fernet
 import os
 import time
 key = b''
+global help_ins
+help_ins = {
+    'delete': 'Delete the current key ',
+    'del': 'Delete the current key ',
+    'change': 'Delete the current key ',
+    'help': 'Get more help',
+    'key': 'Display your key',
+    'encrypt': 'Encrypt your text',
+    'decrypt': 'Decrypt your text'}
 
 def new_password():
     global key
@@ -61,18 +70,30 @@ def decrypting(message):# turn it to bytes
     global key
     try:
         encrypted = message.encode()
-        print(encrypted)
+
         f = Fernet(key)
         decrypted = f.decrypt(encrypted)  # Decrypt the bytes. The returning object is of type bytes
         print("Decrypting successfuly \n\n")
         decrypted = str(decrypted)
-        
 
         return decrypted[2:len(decrypted)-1]
     except Exception as e:
         print("Invalid Key - Unsuccessfully decrypted")
-        print(key)
-        
+
+def getkeys(dict):
+    return list(dict.keys())
+
+
+
+def help():
+
+
+    print("Commands: ")
+    for i in help_ins:
+        print(f'{i} -- {help_ins[i]} ')
+
+    print("\n")
+
 
 if check_key():
     print("Your key has been successfuly imported \n\n")  
@@ -82,26 +103,24 @@ else:
         
 while True:
     if check_key():
+        help_keys = getkeys(help_ins)
         choice = str(input("Do you want to 'encrypt' or 'decrypt' [help] for more?: "))
-        if choice == "encrypt" or choice == "en":
+        if choice.startswith(help_keys[5]) or  choice.startswith('en') :
             message = input("Type the message that you want to encypt: ")
             print(f"Key for word '{message}' is \n {encrypting(message)}")
 
             
-        if choice == "decrypt" or choice == "de":
+        if choice.startswith(help_keys[6]) or  choice.startswith('dec') :
             encrypted_key = input("Enter the encrypted key must in '....': ")
             print(decrypting(encrypted_key))
 
-        if choice == "key" or choice == "my_key" :
+        if choice.startswith(help_keys[4]) or choice == "my key" :
             print("Your key is: {}".format(key))
         
-        if choice.startswith("help"):
-            print("\n\n\n")
-            print("del,delete,change  -  Delete the current key \nhelp  - Get more help \nkey  - Display your key \nencrypt -  Encrypt your text")
-            print("\n\n\n")
+        if choice.startswith(help_keys[3]):
+            help()
+        if choice.startswith(help_keys[0]) or choice.startswith(help_keys[1]) or choice.startswith(help_keys[2]):
 
-        
-        if choice.startswith("del") or choice.startswith("change"):
             while True:
                 ask = input("Are you sure you want to delete your key?: ")
                 ask = ask.lower()
